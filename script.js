ce8e31d8013e3fcfe3bd50c9ce654fa2d40c01c7
Alan Enar <87091720+AlanEnar@users.noreply.github.com>
2023-08-11 17:41:43 -0500

    Fill the array of squares with coordinates

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Testing");
+
+let squares = [];
+let squareSize = 50;// Dimension of the squares in pixels
+
+// Fill the squares array with arrays of coordinates for the squares
+for (var i = 0; i < 16; i++){
+    let x = i * squareSize;
+    let y = (i % 4) * squareSize;
+
+    squares.push([x, y]);
+}
+console.log(squares);
+
+
