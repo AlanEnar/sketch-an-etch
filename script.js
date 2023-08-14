a73b8f689f110d0793e832c826a2bcbe521e6cd1
Alan Enar <87091720+AlanEnar@users.noreply.github.com>
2023-08-14 10:00:58 -0500

    Remove an unneeded line of CSS and do some meaningless tweaks to the JS

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ let squares = [];
 let squareSize = 50;// Dimension of the squares in pixels
 let sketchResolution = 4;// Dimension of the overall sketchpad in number of squares
 
-sketchpad.style.width = squareSize * sketchResolution;// Ensures the right number of squares per row
-
 // Fill the squares array with arrays of coordinates for the squares
 for (var i = 0; i < 16; i++){
     let x = i * squareSize;
@@ -18,10 +16,11 @@ for (var i = 0; i < squares.length; i++){
     let sqrCoordinates = squares[i];
 
     let sqr = document.createElement("div");
-    console.log(sqr);
     sqr.style.width = squareSize;
     sqr.style.height = squareSize;
 
     sketchpad.appendChild(sqr);
 }
 
+sketchpad.style.width = squareSize * sketchResolution;// Ensures the right number of squares per row
+sketchpad.style.height = squareSize * sketchResolution;
