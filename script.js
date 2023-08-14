7ca4226a44a76013a01f53ef1448fc013bf9aaa6
Alan Enar <87091720+AlanEnar@users.noreply.github.com>
2023-08-14 09:55:23 -0500

    Make the squares display - but only in one row

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-
+const sketchpad = document.getElementById("sketchpad");
 let squares = [];
 let squareSize = 50;// Dimension of the squares in pixels
+let sketchResolution = 4;// Dimension of the overall sketchpad in number of squares
+
+sketchpad.style.width = squareSize * sketchResolution;// Ensures the right number of squares per row
 
 // Fill the squares array with arrays of coordinates for the squares
 for (var i = 0; i < 16; i++){
@@ -9,6 +12,16 @@ for (var i = 0; i < 16; i++){
 
     squares.push([x, y]);
 }
-console.log(squares);
 
+// Create divs using the coordinates in the squares array
+for (var i = 0; i < squares.length; i++){
+    let sqrCoordinates = squares[i];
+
+    let sqr = document.createElement("div");
+    console.log(sqr);
+    sqr.style.width = squareSize;
+    sqr.style.height = squareSize;
+
+    sketchpad.appendChild(sqr);
+}
 
