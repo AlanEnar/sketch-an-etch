25d1db152eb5d346d1a64c3b5f28e36b3c1eb899
Alan Enar <87091720+AlanEnar@users.noreply.github.com>
2023-08-15 18:29:10 -0500

    Make the width & height settings in the js file actually do something

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const sketchpad = document.getElementById("sketchpad");
 let squares = [];
-let squareSize = 50;// Dimension of the squares in pixels
-let sketchResolution = 4;// Dimension of the overall sketchpad in number of squares
+let squareSize = 25;// Dimension of the squares in pixels
+let sketchResolution = 16;// Dimension of the overall sketchpad in number of squares
 
 // Fill the squares array with arrays of coordinates for the squares
 for (var i = 0; i < 16; i++){
@@ -16,11 +16,12 @@ for (var i = 0; i < squares.length; i++){
     let sqrCoordinates = squares[i];
 
     let sqr = document.createElement("div");
-    sqr.style.width = squareSize;
-    sqr.style.height = squareSize;
-
+    sqr.classList.add("square");
     sketchpad.appendChild(sqr);
+
 }
 
-sketchpad.style.width = squareSize * sketchResolution;// Ensures the right number of squares per row
-sketchpad.style.height = squareSize * sketchResolution;
+
+
+sketchpad.style.width = squareSize * sketchResolution + "px";// Ensures the right number of squares per row
+sketchpad.style.height = squareSize * sketchResolution + "px";
