6640a4beb1c48a3cc76ae1ea084ee5612b3b16b7
Alan Enar <87091720+AlanEnar@users.noreply.github.com>
2023-08-15 18:46:20 -0500

    Clean out unneeded lines

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
 const sketchpad = document.getElementById("sketchpad");
-let squares = [];
-let squareSize = 50;// Dimension of the squares in pixels
+let squareSize = 25;// Dimension of the squares in pixels
 let sketchResolution = 8;// Dimension of the overall sketchpad in number of squares
 let squareCount = sketchResolution * sketchResolution;// Number of squares
 
-// Fill the squares array with arrays of coordinates for the squares
-for (var i = 0; i < 16; i++){
-    let x = i * squareSize;
-    let y = (i % 4) * squareSize;
-
-    squares.push([x, y]);
-}
-
 // Create the divs needed to fill the pad
 for (var i = 0; i < squareCount; i++){
-    let sqrCoordinates = squares[i];
-
     let sqr = document.createElement("div");
-    //sqr.classList.add("square");
+
+    sqr.classList.add("square");
     sqr.style.width = squareSize + "px";
     sqr.style.height = squareSize + "px";
 
