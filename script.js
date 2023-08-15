e1debae31fb13bd01751b1d20a4f2835761d4341
Alan Enar <87091720+AlanEnar@users.noreply.github.com>
2023-08-15 18:37:26 -0500

    Make the squares actually show up... as squares

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const sketchpad = document.getElementById("sketchpad");
 let squares = [];
-let squareSize = 25;// Dimension of the squares in pixels
+let squareSize = 50;// Dimension of the squares in pixels
 let sketchResolution = 16;// Dimension of the overall sketchpad in number of squares
 
 // Fill the squares array with arrays of coordinates for the squares
@@ -16,9 +16,12 @@ for (var i = 0; i < squares.length; i++){
     let sqrCoordinates = squares[i];
 
     let sqr = document.createElement("div");
-    sqr.classList.add("square");
-    sketchpad.appendChild(sqr);
+    //sqr.classList.add("square");
+    sqr.style.width = squareSize + "px";
+    sqr.style.height = squareSize + "px";
+    //sqr.textContent = i;
 
+    sketchpad.appendChild(sqr);
 }
 
 
