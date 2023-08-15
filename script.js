cef0d8d3e35f116d2b633bbccef41759c920945e
Alan Enar <87091720+AlanEnar@users.noreply.github.com>
2023-08-15 18:51:53 -0500

    Add mouseMoved function without functionality

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//
+// Sketchpad Setup
+//
 const sketchpad = document.getElementById("sketchpad");
 let squareSize = 25;// Dimension of the squares in pixels
 let sketchResolution = 8;// Dimension of the overall sketchpad in number of squares
@@ -18,3 +21,14 @@ for (var i = 0; i < squareCount; i++){
 // Assign sketchpad dimensions
 sketchpad.style.maxWidth = squareSize * sketchResolution + "px";// Ensures the right number of squares per row
 sketchpad.style.maxHeight = squareSize * sketchResolution + "px";
+
+//
+// User Interaction
+//
+
+function mouseMoved (){
+    //console.log("mouse over pad");
+}
+
+
+sketchpad.addEventListener("mousemove", mouseMoved());
